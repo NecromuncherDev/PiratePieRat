1fe0ac0dddab634554ca8fb4c7cba60f09178871
NecromuncherDev <Oded56@gmail.com>
2023-02-06 13:06:29 +0200

    Copied PoolManager from Shahar's project

--- Assets/_PPR/Scripts/Core/PPRPoolManager.cs
+++ Assets/_PPR/Scripts/Core/PPRPoolManager.cs
@@ -8,55 +8,109 @@ namespace PPR.Core
     {
         private Dictionary<string, PPRPool> Pools = new();
 
-        void InitPool<T>(T original, int amount, int maxAmount) where T : Object, IPPRPoolable
+        public void InitPool(IPPRPoolable original, int amount, int maxAmount)
         {
-            PPRManager.Instance.FactoryManager.MultiCreate<T>(original, Vector3.zero, amount, 
-                delegate(List<T> list)
+            PPRManager.Instance.FactoryManager.MultiCreate(original, Vector3.zero, amount,
+                delegate (List<IPPRPoolable> list)
                 {
+                    foreach (var poolable in list)
+                    {
+                        poolable.name = original.name;
+                    }
+
                     var pool = new PPRPool
                     {
-                        AllPoolables = new List<IPPRPoolable>(list),
-                        UsedPoolables = new List<IPPRPoolable>(),
-                        AvailablePoolables = new List<IPPRPoolable>(list),
+                        AllPoolables = new Queue<IPPRPoolable>(list),
+                        UsedPoolables = new Queue<IPPRPoolable>(),
+                        AvailablePoolables = new Queue<IPPRPoolable>(list),
                         MaxPoolables = maxAmount
                     };
 
-                    Pools.Add(original.name, pool);
+                    Pools.Add(original.gameObject.name, pool);
                 });
         }
 
         public IPPRPoolable GetPoolable(string poolName)
         {
-            
-            if (Pools.TryGetValue(poolName, out var retPool))
+            if (Pools.TryGetValue(poolName, out PPRPool pool))
             {
-                
+                if (pool.AvailablePoolables.TryDequeue(out IPPRPoolable poolable))
+                {
+                    poolable.OnTakenFromPool();
+
+                    pool.UsedPoolables.Enqueue(poolable);
+                    return poolable;
+                }
+
+                //Create more
+                Debug.Log($"pool - {poolName} no enough poolables, used poolables {pool.UsedPoolables.Count}");
+
+                return null;
             }
 
+            Debug.Log($"pool - {poolName} wasn't initialized");
             return null;
         }
 
-        public void ReturnPoolable<T>(string poolName, T original) where T : Object, IPPRPoolable
-        {
 
+        public void ReturnPoolable(IPPRPoolable poolable)
+        {
+            if (Pools.TryGetValue(poolable.poolName, out PPRPool pool))
+            {
+                pool.AvailablePoolables.Enqueue(poolable);
+                poolable.OnReturnedToPool();
+            }
         }
 
-        void DestroyPool()
+
+        public void DestroyPool(string name)
         {
-            
+            if (Pools.TryGetValue(name, out PPRPool pool))
+            {
+                foreach (var poolable in pool.AllPoolables)
+                {
+                    poolable.PreDestroy();
+                    ReturnPoolable(poolable);
+                }
+
+                foreach (var poolable in pool.AllPoolables)
+                {
+                    Object.Destroy(poolable);
+                }
+
+                pool.AllPoolables.Clear();
+                pool.AvailablePoolables.Clear();
+                pool.UsedPoolables.Clear();
+
+                Pools.Remove(name);
+            }
         }
     }
 
-    public interface IPPRPoolable
+    public class IPPRPoolable : PPRMonoBehaviour
     {
-        
+        public string poolName;
+
+        public virtual void OnReturnedToPool()
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        public virtual void OnTakenFromPool()
+        {
+            this.gameObject.SetActive(true);
+        }
+
+        public virtual void PreDestroy()
+        {
+        }
     }
 
-    public class PPRPool 
+    public class PPRPool
     {
-        public List<IPPRPoolable> AllPoolables = new();
-        public List<IPPRPoolable> UsedPoolables = new();
-        public List<IPPRPoolable> AvailablePoolables = new();
+        public Queue<IPPRPoolable> AllPoolables = new();
+        public Queue<IPPRPoolable> UsedPoolables = new();
+        public Queue<IPPRPoolable> AvailablePoolables = new();
 
         public int MaxPoolables = 100;
     }
